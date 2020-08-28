df96f37b1e0442e18061a582b0a1c4635a5bfe13
Saulo G. Felix <meetnapoleon@protonmail.ch>
2020-08-28 01:04:30 -0300

    Grouped detection rectangles

--- pca.py
+++ pca.py
@@ -1,6 +1,6 @@
+import time
 import numpy as np
 import matplotlib.pyplot as plt
-import time
 import cv2
 
 STAGE = 'gf_stage.png'
@@ -14,7 +14,7 @@ def PCA(M, k=2):
     if (x1 >= x2): M = M.T
     R = np.cov(M - mean(M))
     lamb, Vy = np.linalg.eigh(R)
-    return lamb, M.T.dot(Vy)[:,:k]
+    return lamb, Vy[:,:k]
 
 def imshow(A, grid=True):
     fig, axs = plt.subplots(1, len(A))
@@ -30,7 +30,7 @@ def linfit(set):
 def euclidian_dist(V, Vt=None):
     return np.linalg.norm(V - Vt)
 
-def set_plt_params(figsize=[8,5], figdpi=150, style='default'):
+def set_plt_params(figsize=[5,5], figdpi=150, style='default'):
     plt.rcParams['figure.figsize'] = figsize
     plt.rcParams['figure.dpi'] = figdpi
     plt.style.use(style)
@@ -63,67 +63,58 @@ if __name__ == '__main__':
     set_plt_params(style='dark_background', figdpi=200)
 
     w, h = (60, 80)
-    img = cv2.imread(STAGE, cv2.IMREAD_GRAYSCALE)
+    img = cv2.imread(STAGE, cv2.IMREAD_GRAYSCALE) / 255
     notes = [NOTE1, NOTE2, NOTE3, NOTE4]
     A = np.zeros([len(notes), w*h])
 
     for index, note in enumerate(notes):
-        imgc = cv2.imread(note, cv2.IMREAD_GRAYSCALE)
+        imgc = cv2.imread(note, cv2.IMREAD_GRAYSCALE) / 255
         A[index:w*h] = imgc.reshape(w*h)
 
-    lamb, V = PCA(A.T, k=2)
-    U = mean(V.T)
+    lamb, V = PCA(A.T, k=4)
+    U = normalize(mean(V.T.dot(A)))
 
     W, H = (840, 560)
     img_pad = np.zeros((W, H))
     img_pad[:img.shape[0],:img.shape[1]] = img
     j = 0; leng = int(W*H/(w*h))
-    count = 0
 
-    print(img_pad.shape)
-
-    plt.ion()
-
-    fig, axs = plt.subplots()
-    im1 = axs.imshow(img)
-
-    stepx, stepy = (10, 30)
-    maxx = int(W/stepx) - 20
+    stepx, stepy = (20, 30)
+    maxx = int(W/stepx) - 10
     maxy = int(H/stepy) - 1
+
     d = np.zeros(maxx*maxy); k = 0
+    locations = []
+
+    start = time.time()
 
     for i in range(maxx):
+        dx = (stepx*i, w + (stepx*i))
         for j in range(maxy):
-            dx = (stepx*i, w + (stepx*i))
             dy = (stepy*j, h + (stepy*j))
-            im = img_pad[dx[0]:dx[1],dy[0]:dy[1]]
+            im = img_pad[dx[0]:dx[1], dy[0]:dy[1]]
 
-            L = mean(U * (im.reshape(w*h) - mean(A)))
+            L = normalize(mean(U * (im.reshape(w*h) - mean(A))))
             dist = euclidian_dist(U, L)
 
-            print('D = {:.2f}'.format(dist))
-            print('Maping = ({}, {})'.format(i, j))
-
             d[k] = dist; k += 1
-            norm = 1e6
-            threshold = 0.93
+            threshold = 1.925
 
-            if dist < threshold*norm:
-                count += 1
-                pt = (dy[0], dx[0])
+            if dist < threshold:
+                locations.append([dy[0], dx[0] - 10, int(w*1.2), int(h)])
+                locations.append([dy[0], dx[0], int(w), int(h)])
 
-                print('FOUND!')
-                print('Coords (x, y) = ({}, {})'.format(pt[0],pt[1]))
-                print('------')
+    rects, _ = cv2.groupRectangles(locations, 1, 0.4)
 
-                cv2.rectangle(img, pt, (pt[0] + h, pt[1] + w), (255,255,255), 3)
-                im1.set_data(img)
-            fig.canvas.flush_events()
-            # time.sleep(0.5)
+    print('Elapsed time for 1 image = {:.2f}s'.format(time.time() - start))
+    print('FPS = {:.1f}'.format(1/(time.time() - start)))
+    print('Found {} points'.format(len(d[d < threshold])))
+    print('Grouped {} rectangles'.format(len(rects)))
 
-    print('Found {} notes'.format(count))
+    for (x, y, w, h) in rects:
+        top_left = (x, y)
+        bottom_right = (x + h, y + w)
+        cv2.rectangle(img, top_left, bottom_right, (255,255,255), 3)
 
-    # pt = (240,480)
-    # cv2.rectangle(img, pt, (pt[0] + h, pt[1] + w), (255,255,255), 3)
-    # cv2.imshow('', img)
-    # cv2.waitKey()
+    cv2.imshow('', img)
+    cv2.waitKey()
